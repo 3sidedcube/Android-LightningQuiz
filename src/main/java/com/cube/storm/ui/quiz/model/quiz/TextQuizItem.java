f2a5e997a446eb562957dba268c6748ea83918c6
Callum Taylor <callum@3sidedcube.com>
2014-11-19 11:47:33 +0000

    Added base text quiz item

---
+++ src/main/java/com/cube/storm/ui/quiz/model/quiz/TextQuizItem.java
@@ -0,0 +1,18 @@
+package com.cube.storm.ui.quiz.model.quiz;
+
+import com.cube.storm.ui.model.property.TextProperty;
+
+import java.util.Collection;
+
+import lombok.Getter;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project LightningQuiz
+ */
+public abstract class TextQuizItem extends ItemQuizItem
+{
+	@Getter protected Collection<TextProperty> options;
+}
