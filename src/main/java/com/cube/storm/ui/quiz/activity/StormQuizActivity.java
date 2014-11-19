0ab9ba109dbb73f9a695f11f9a490e24a407993b
Callum Taylor <callum@3sidedcube.com>
2014-11-19 09:56:45 +0000

    Added base quiz activity and view

---
+++ src/main/java/com/cube/storm/ui/quiz/activity/StormQuizActivity.java
@@ -0,0 +1,20 @@
+package com.cube.storm.ui.quiz.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.cube.storm.ui.activity.StormActivity;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project Storm
+ */
+public class StormQuizActivity extends StormActivity
+{
+	@Override public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+	{
+		super.onCreate(savedInstanceState, persistentState);
+	}
+}
