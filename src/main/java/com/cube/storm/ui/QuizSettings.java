a3187504ea3d5813b7862c60aeb54fd527d1bd19
Callum Taylor <callum@3sidedcube.com>
2014-11-19 09:32:25 +0000

    Added basic quiz settings class and custom intent factory override

---
+++ src/main/java/com/cube/storm/ui/QuizSettings.java
@@ -0,0 +1,98 @@
+package com.cube.storm.ui;
+
+import com.cube.storm.UiSettings;
+import com.cube.storm.ui.lib.factory.IntentFactory;
+import com.cube.storm.ui.quiz.lib.QuizIntentFactory;
+
+/**
+ * Settings class for the quiz module.
+ * <p/>
+ * This class is used for setting up the module to enable the use of Quizes and Quiz objects in Storm. This class must be
+ * instantiated <b>after</b> the {@link com.cube.storm.UiSettings} has been created as this class requires a reference
+ * of the settings class to be passed to it to ensure that it can properly parse the right objects/models into the right
+ * classes.
+ * <p/>
+ * To enable the use of the library, you must instantiate
+ * a new {@link QuizSettings.Builder} object in your {@link android.app.Application} singleton class.
+ * <p/>
+ * This class should not be directly instantiated.
+ *
+ * @author Callum Taylor
+ * @project LightningQuiz
+ */
+public class QuizSettings
+{
+	/**
+	 * The singleton instance of the settings
+	 */
+	private static QuizSettings instance;
+
+	/**
+	 * Gets the instance of the {@link QuizSettings} class
+	 * Throws a {@link java.lang.IllegalAccessError} if the singleton has not been instantiated properly
+	 *
+	 * @return The instance
+	 */
+	public static QuizSettings getInstance()
+	{
+		if (instance == null)
+		{
+			throw new IllegalAccessError("You must build the QuizSettings settings object first using QuizSettings$Builder");
+		}
+
+		return instance;
+	}
+
+	/**
+	 * Default private constructor
+	 */
+	private QuizSettings(){}
+
+	/**
+	 * The builder class for {@link com.cube.storm.UiSettings}. Use this to create a new {@link com.cube.storm.UiSettings} instance
+	 * with the customised properties specific for your project.
+	 *
+	 * Call {@link #build()} to build the settings object.
+	 */
+	public static class Builder
+	{
+		/**
+		 * The temporary instance of the {@link com.cube.storm.ui.QuizSettings} object.
+		 */
+		private QuizSettings construct;
+
+		/**
+		 * The temporary instance of the {@link com.cube.storm.UiSettings} object to inject into.
+		 */
+		private UiSettings uiSettings;
+
+		/**
+		 * Default constructor
+		 *
+		 * @param uiSettings The settings object to use to enable quiz module
+		 */
+		public Builder(UiSettings uiSettings)
+		{
+//			uiSettings.getViewProcessors().put(QuizQuestion.class, )
+			intentFactory(new QuizIntentFactory(uiSettings.getIntentFactory()));
+		}
+
+		public Builder intentFactory(IntentFactory intentFactory)
+		{
+			uiSettings.setIntentFactory(intentFactory);
+
+			return this;
+		}
+
+		/**
+		 * Builds the final settings object and sets its instance. Use {@link #getInstance()} to retrieve the settings
+		 * instance.
+		 *
+		 * @return The newly set {@link com.cube.storm.UiSettings} instance
+		 */
+		public QuizSettings build()
+		{
+			return (QuizSettings.instance = construct);
+		}
+	}
+}
---
+++ src/main/java/com/cube/storm/ui/quiz/lib/QuizIntentFactory.java
@@ -0,0 +1,72 @@
+package com.cube.storm.ui.quiz.lib;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.cube.storm.ui.data.FragmentIntent;
+import com.cube.storm.ui.lib.factory.IntentFactory;
+import com.cube.storm.ui.model.descriptor.PageDescriptor;
+
+/**
+ * Delegate intent factory used to inject quiz functionality into ui settings module.
+ * <p/>
+ * This module overrides the standard {@link com.cube.storm.UiSettings} intent factory and adds small
+ * additional functionality to handle quiz classes. The original {@link com.cube.storm.ui.lib.factory.IntentFactory} is retained
+ * to allow hierarchy completion.
+ *
+ * @author Callum Taylor
+ * @project LightningQuiz
+ */
+public class QuizIntentFactory extends IntentFactory
+{
+	private IntentFactory superFactory;
+
+	public QuizIntentFactory(IntentFactory superFactory)
+	{
+		this.superFactory = superFactory;
+	}
+
+	@Nullable @Override public FragmentIntent getFragmentIntentForPageDescriptor(@NonNull PageDescriptor pageDescriptor)
+	{
+		FragmentIntent ret = super.getFragmentIntentForPageDescriptor(pageDescriptor);
+//		Class<? extends Model> pageType = UiSettings.getInstance().getViewFactory().getModelForView(pageDescriptor.getType());
+//
+//		if (QuizPage.class.isAssignableFrom(pageType))
+//		{
+//			if (ret != null)
+//			{
+//				ret.setFragment(StormQuizFragment.class);
+//			}
+//			else
+//			{
+//				Bundle args = new Bundle();
+//				args.putSerializable(StormActivity.EXTRA_URI, pageDescriptor.getSrc());
+//
+//				ret = new FragmentIntent(StormQuizFragment.class, null, args);
+//			}
+//		}
+
+		return ret;
+	}
+
+	@Nullable @Override public Intent getIntentForPageDescriptor(@NonNull Context context, @NonNull PageDescriptor pageDescriptor)
+	{
+		Intent ret = super.getIntentForPageDescriptor(context, pageDescriptor);
+
+//		Class<? extends Model> pageType = UiSettings.getInstance().getViewFactory().getModelForView(pageDescriptor.getType());
+//
+//		if (pageType != null)
+//		{
+//			if (Quiz.class.isAssignableFrom(pageType))
+//			{
+//				Bundle extras = ret.getExtras();
+//				ret = new Intent(context, StormQuizActivity.class);
+//				ret.putExtras(extras);
+//			}
+//		}
+
+		return ret;
+	}
+}
