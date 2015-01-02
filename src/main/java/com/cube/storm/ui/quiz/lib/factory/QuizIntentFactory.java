ca6ccaf9417d1b5d6c578023eed980b6d5803542
Callum Taylor <callum@3sidedcube.com>
2015-01-02 10:52:06 +0000

    Fixed issue with intent factory not using super factory for its super calls causing bad intents to be returned when using the library

--- src/main/java/com/cube/storm/ui/quiz/lib/factory/QuizIntentFactory.java
+++ src/main/java/com/cube/storm/ui/quiz/lib/factory/QuizIntentFactory.java
@@ -2,6 +2,7 @@ package com.cube.storm.ui.quiz.lib.factory;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +13,7 @@ import com.cube.storm.ui.data.FragmentIntent;
 import com.cube.storm.ui.lib.factory.IntentFactory;
 import com.cube.storm.ui.model.Model;
 import com.cube.storm.ui.model.descriptor.PageDescriptor;
+import com.cube.storm.ui.model.page.Page;
 import com.cube.storm.ui.quiz.activity.StormQuizActivity;
 import com.cube.storm.ui.quiz.fragment.StormQuizFragment;
 import com.cube.storm.ui.quiz.model.page.QuizPage;
@@ -37,7 +39,7 @@ public class QuizIntentFactory extends IntentFactory
 
 	@Nullable @Override public FragmentIntent getFragmentIntentForPageDescriptor(@NonNull PageDescriptor pageDescriptor)
 	{
-		FragmentIntent ret = super.getFragmentIntentForPageDescriptor(pageDescriptor);
+		FragmentIntent ret = superFactory.getFragmentIntentForPageDescriptor(pageDescriptor);
 		Class<? extends Model> pageType = UiSettings.getInstance().getViewFactory().getModelForView(pageDescriptor.getType());
 
 		if (pageType != null)
@@ -63,7 +65,7 @@ public class QuizIntentFactory extends IntentFactory
 
 	@Nullable @Override public Intent getIntentForPageDescriptor(@NonNull Context context, @NonNull PageDescriptor pageDescriptor)
 	{
-		Intent ret = super.getIntentForPageDescriptor(context, pageDescriptor);
+		Intent ret = superFactory.getIntentForPageDescriptor(context, pageDescriptor);
 		Class<? extends Model> pageType = UiSettings.getInstance().getViewFactory().getModelForView(pageDescriptor.getType());
 
 		if (pageType != null)
@@ -77,5 +79,25 @@ public class QuizIntentFactory extends IntentFactory
 		}
 
 		return ret;
+	}
+
+	@Nullable @Override public FragmentIntent getFragmentIntentForPage(@NonNull Page pageData)
+	{
+		return superFactory.getFragmentIntentForPage(pageData);
+	}
+
+	@Nullable @Override public Intent getIntentForPage(@NonNull Context context, @NonNull Page pageData)
+	{
+		return superFactory.getIntentForPage(context, pageData);
+	}
+
+	@Nullable @Override public FragmentIntent getFragmentIntentForPageUri(@NonNull Uri pageUri)
+	{
+		return superFactory.getFragmentIntentForPageUri(pageUri);
+	}
+
+	@Nullable @Override public Intent geIntentForPageUri(@NonNull Context context, @NonNull Uri pageUri)
+	{
+		return superFactory.geIntentForPageUri(context, pageUri);
 	}
 }
