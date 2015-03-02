59a757ccc832640cc152ccd0edada95fd42a7e74
Callum Taylor <callum@3sidedcube.com>
2015-03-02 16:10:48 +0000

    Added basic slider quiz item class

---
+++ src/main/java/com/cube/storm/ui/quiz/model/quiz/SliderQuizItem.java
@@ -0,0 +1,23 @@
+package com.cube.storm.ui.quiz.model.quiz;
+
+import com.cube.storm.ui.model.property.ImageProperty;
+import com.cube.storm.ui.model.property.TextProperty;
+import com.cube.storm.ui.quiz.model.property.RangeProperty;
+
+import lombok.Getter;
+
+/**
+ * Model for SliderQuizItem class
+ *
+ * @author Callum Taylor
+ * @project LightningQuiz
+ */
+public class SliderQuizItem extends ItemQuizItem
+{
+	@Getter protected ImageProperty image;
+	@Getter protected RangeProperty range;
+	@Getter protected TextProperty unit;
+	@Getter protected int answer;
+	@Getter protected int initialPosition;
+	@Getter protected boolean correct = false;
+}
