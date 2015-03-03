8d86241c499e8c8f823ae62f29185a2c87704449
Callum Taylor <callum@3sidedcube.com>
2015-03-03 14:47:32 +0000

    Added slider functionality and correct checker for slider quiz item

--- src/main/java/com/cube/storm/ui/quiz/view/holder/SliderQuizItemHolder.java
+++ src/main/java/com/cube/storm/ui/quiz/view/holder/SliderQuizItemHolder.java
@@ -1,16 +1,22 @@
 package com.cube.storm.ui.quiz.view.holder;
 
+import android.graphics.Bitmap;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.cube.storm.UiSettings;
+import com.cube.storm.ui.model.property.ImageProperty;
 import com.cube.storm.ui.quiz.R;
 import com.cube.storm.ui.quiz.model.quiz.SliderQuizItem;
 import com.cube.storm.ui.view.holder.ViewHolder;
 import com.cube.storm.ui.view.holder.ViewHolderController;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 /**
  * View holder for {@link com.cube.storm.ui.quiz.model.quiz.SliderQuizItem} in the adapter
@@ -32,6 +38,11 @@ public class SliderQuizItemHolder extends ViewHolderController
 	{
 		protected TextView title;
 		protected TextView hint;
+		protected ImageView image;
+		protected SeekBar slider;
+		protected TextView sliderText;
+
+		protected int pos = Integer.MIN_VALUE;
 
 		public SliderQuizItemViewHolder(View view)
 		{
@@ -39,10 +50,20 @@ public class SliderQuizItemHolder extends ViewHolderController
 
 			title = (TextView)view.findViewById(R.id.title);
 			hint = (TextView)view.findViewById(R.id.hint);
+			image = (ImageView)view.findViewById(R.id.image);
+			slider = (SeekBar)view.findViewById(R.id.slider);
+			sliderText = (TextView)view.findViewById(R.id.slider_text);
 		}
 
 		@Override public void populateView(SliderQuizItem model)
 		{
+			slider.setOnSeekBarChangeListener(new ModelSeekBarListener(model));
+
+			if (pos < 0)
+			{
+				pos = model.getInitialPosition() - model.getRange().getStart();
+			}
+
 			title.setVisibility(View.GONE);
 			hint.setVisibility(View.GONE);
 
@@ -75,6 +96,86 @@ public class SliderQuizItemHolder extends ViewHolderController
 					hint.setVisibility(View.GONE);
 				}
 			}
+
+			image.setImageBitmap(null);
+			UiSettings.getInstance().getImageLoader().cancelDisplayTask(image);
+
+			final ImageProperty imageProperty = model.getImage();
+			if (imageProperty != null)
+			{
+				UiSettings.getInstance().getImageLoader().displayImage(imageProperty.getSrc(), image, new SimpleImageLoadingListener()
+				{
+					@Override public void onLoadingStarted(String imageUri, View view)
+					{
+						image.setVisibility(View.INVISIBLE);
+					}
+
+					@Override public void onLoadingFailed(String imageUri, View view, FailReason failReason)
+					{
+						if (!imageUri.equalsIgnoreCase(imageProperty.getFallbackSrc()))
+						{
+							UiSettings.getInstance().getImageLoader().displayImage(imageProperty.getFallbackSrc(), image, this);
+						}
+
+						image.setVisibility(View.VISIBLE);
+					}
+
+					@Override public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage)
+					{
+						image.setVisibility(View.VISIBLE);
+					}
+				});
+			}
+
+			if (model.getRange() != null)
+			{
+				slider.setMax(model.getRange().getLength() - 1);
+				slider.setProgress(pos);
+
+				if (model.getTitle() != null)
+				{
+					String content = UiSettings.getInstance().getTextProcessor().process(model.getUnit().getContent());
+
+					if (!TextUtils.isEmpty(content))
+					{
+						sliderText.setText("" + (pos + model.getRange().getStart()) + " " + content);
+					}
+				}
+			}
+		}
+
+		private class ModelSeekBarListener implements SeekBar.OnSeekBarChangeListener
+		{
+			private SliderQuizItem model;
+
+			private ModelSeekBarListener(SliderQuizItem model)
+			{
+				this.model = model;
+			}
+
+			@Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+			{
+				if (fromUser)
+				{
+					pos = progress;
+					model.setCorrect((progress + model.getRange().getStart()) == model.getAnswer());
+
+					TextView sliderText = (TextView)((View)seekBar.getParent()).findViewById(R.id.slider_text);
+
+					if (model.getTitle() != null)
+					{
+						String content = UiSettings.getInstance().getTextProcessor().process(model.getUnit().getContent());
+
+						if (!TextUtils.isEmpty(content))
+						{
+							sliderText.setText("" + (pos + model.getRange().getStart()) + " " + content + " : " + model.isCorrect());
+						}
+					}
+				}
+			}
+
+			@Override public void onStartTrackingTouch(SeekBar seekBar){}
+			@Override public void onStopTrackingTouch(SeekBar seekBar){}
 		}
 	}
 }
