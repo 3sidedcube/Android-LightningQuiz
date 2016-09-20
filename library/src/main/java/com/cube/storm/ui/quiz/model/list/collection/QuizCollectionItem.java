b0bd6133703f0782a07d6df27ecf75718b96912b
Callum Taylor <callum@3sidedcube.com>
2016-09-20 10:55:11 +0100

    Changed quiz collection inheritance

--- library/src/main/java/com/cube/storm/ui/quiz/model/list/collection/QuizCollectionItem.java
+++ library/src/main/java/com/cube/storm/ui/quiz/model/list/collection/QuizCollectionItem.java
@@ -2,7 +2,6 @@ package com.cube.storm.ui.quiz.model.list.collection;
 
 import android.os.Parcel;
 
-import com.cube.storm.ui.model.list.collection.CollectionItem;
 import com.cube.storm.ui.model.property.LinkProperty;
 
 import lombok.AllArgsConstructor;
@@ -10,18 +9,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-/**
- * // TODO: Add class description
- *
- * @author Alan Le Fournis
- * @project Storm
- */
 @NoArgsConstructor @AllArgsConstructor(suppressConstructorProperties = true)
- @Accessors(chain = true) @Data
-public class QuizCollectionItem extends CollectionItem
+@Accessors(chain = true) @Data
+public class QuizCollectionItem extends BadgeCollectionItem
 {
 	protected LinkProperty quiz;
-	protected String badgeId;
 
 	@Override public int describeContents()
 	{
